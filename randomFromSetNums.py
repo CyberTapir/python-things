bcb9c7ee2a320f4cca9379b63d1b7eb5380cf25d
Callum <66990734+CyberTapir@users.noreply.github.com>
2024-06-04 08:31:15 +1000

    update
    It should work

--- randomFromSetNums.py
+++ randomFromSetNums.py
@@ -5,6 +5,8 @@
 # Last Modified Date: 03.06.2024
 # Last Modified By  : Callum Bennett
 
+array = []
+
 def randomNumbers():
     num1 = input("Enter the first number: ")
     num2 = input("Enter the second number: ")
@@ -12,15 +14,15 @@ def randomNumbers():
     num4 = input("Enter the fourth number: ")
     num5 = input("Enter the fifth number: ")
     num6 = input("Enter the sixth number: ")
-    
-    if num1.isNumeric():
-        if num2.IsNumeric():
-            if num3.IsNumeric():
-                if num4.IsNumeric():
-                    if num5.IsNumeric():
-                        if num6.IsNumeric():
+
+    if num1.isnumeric == True:
+        if num2.isnumeric == True:
+            if num3.isnumeric == True:
+                if num4.isnumeric == True:
+                    if num5.isnumeric == True:
+                        if num6.isnumeric == True:
                             print("All numbers are numeric")
-                            generateRandomNumbers(makeArray(num1, num2, num3, num4, num5, num6))
+                            array = generateRandomNumbers(makeArray(num1, num2, num3, num4, num5, num6))
 
 
 def makeArray(num1, num2, num3, num4, num5, num6):
@@ -32,7 +34,7 @@ def makeArray(num1, num2, num3, num4, num5, num6):
     inputNumbers.append(num5)
     inputNumbers.append(num6)
     return inputNumbers
-        
+
 def generateRandomNumbers(inputNums):
     import random
     randomNumbers = []
@@ -42,3 +44,17 @@ def generateRandomNumbers(inputNums):
             currentNum = currentNum & inputNums(random.randint(0, 5))
         randomNumbers.append(currentNum)
     return randomNumbers
+
+def howManyColumns():
+    columns = input("How many columns would you like to display the numbers in? ")
+    return columns
+
+def displayNumbers(randomNumbers, columns):
+    for i in range(0, 99):
+        if i % columns == 0:
+            print("\n")
+        print(randomNumbers(i) + " ")
+    print("\n")
+
+randomNumbers()
+displayNumbers(array, howManyColumns())
